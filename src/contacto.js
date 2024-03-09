afe12435e9cff5dbd7dc20d5b1a68ff419026f7b
vicentediaz2 <vicentedaniel.diaz2@gmail.com>
2024-03-08 23:15:51 -0300

    first commit

--- src/contacto.js
+++ src/contacto.js
@@ -5,19 +5,19 @@ function Contacto() {
         <div className="Contacto">
             <div>
                 <p className='Contacto-titulo Punto'>
-                    Contacto <span className='Punto'>.</span>
+                    Contacto .
                 </p>
                 <p className='Contacto-texto'>
-                    <img src="svg_email.svg" className='svg'></img>&emsp; vicentedaniel.diaz2@gmail.com
+                    <img src="svg_email.svg" className='svg' alt='email'></img>&emsp; vicentedaniel.diaz2@gmail.com
                 </p>
                 <p className='Contacto-texto'>
-                    <img src="svg_telephone.svg" className='svg'></img>&emsp; +56 9 6572 7136
+                    <img src="svg_telephone.svg" className='svg' alt='telefono'></img>&emsp; +56 9 6572 7136
                 </p>
                 <p className='Contacto-texto'>
-                    <img src="svg_github.svg" className='svg'></img>&emsp; https://github.com/vicentediaz2
+                    <img src="svg_github.svg" className='svg' alt='GitHub'></img>&emsp; https://github.com/vicentediaz2
                 </p>
                 <p className='Contacto-texto'>
-                    <img src="svg_globe.svg" className='svg'></img>&emsp; https://vicentediaz2.github.io/cv
+                    <img src="svg_globe.svg" className='svg' alt='Paguina Web'></img>&emsp; https://vicentediaz2.github.io/cv
                 </p>
             </div>
             <img className='imagen' src='contacto.jpg' alt='contacto'></img>
